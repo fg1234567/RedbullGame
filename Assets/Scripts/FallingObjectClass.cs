c697ddc8a36949cf1c47d4c1bdd19225185fbe04
gokhanhalkman <gokhanhalkman@gmail.com>
2018-11-23 17:47:20 +0600

    created prefabs, initialized game one

---
+++ Assets/Scripts/FallingObjectClass.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectClass {//6 variables
+
+    GameObject startReference;//ref1 
+    GameObject targetReference; //bar
+    GameObject RedbullCan_def, RedbullCan_anim; //defaultRedbullCan, flyingRedbulCan
+    float fallingVelocity;
+    Vector3 tempPosition;
+    bool eventler = false;
+
+    public FallingObjectClass(GameObject _startReference, GameObject _targetReference, GameObject _RedbullCan_def, GameObject _RedbullCan_anim, float _fallingVelocity, Vector3 _tempPosition)
+    {
+        startReference = _startReference;
+        targetReference = _targetReference;
+        RedbullCan_def = _RedbullCan_def;
+        RedbullCan_anim = _RedbullCan_anim;
+        fallingVelocity = _fallingVelocity;
+        tempPosition = _tempPosition;
+        instantiate();
+    }
+
+    void instantiate()
+    {
+        GameObject.Instantiate(RedbullCan_def, startReference.transform.position, Quaternion.identity);
+        tempPosition = startReference.transform.position;
+    }
+
+    public void loop()
+    {
+        if (!eventler)
+        {
+            tempPosition.y += (-1) * Time.deltaTime;
+            RedbullCan_def.transform.position = tempPosition;
+        }
+        else
+        {
+            var diffofx = targetReference.transform.position.x - tempPosition.x;
+            var diffofy = targetReference.transform.position.y - tempPosition.y;
+
+            var distance = Mathf.Sqrt(diffofx * diffofx + diffofy * diffofy);
+
+            tempPosition.x += diffofx*fallingVelocity*Time.deltaTime / distance;
+            tempPosition.y += diffofy*fallingVelocity*Time.deltaTime / distance;
+            RedbullCan_anim.transform.position = tempPosition;
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(RedbullCan_anim.transform.position);
+            Debug.Log(RedbullCan_def.transform.position);
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Checks whether the mouse click hits a mesh collider
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    if(hit.transform.gameObject.tag == "RedbullCan_Default")
+                    {
+                        eventler = true;
+
+                        GameObject.Destroy(hit.transform.gameObject);
+                        GameObject.Instantiate(RedbullCan_anim, hit.transform.position, Quaternion.identity);
+                    }
+                    
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public GameObject defaultRedbullCan, flyingRedbulCan;
+    public GameObject ref1, bar;
+    //public Animator triggered;
+    Vector3 tempPosition;
+    Vector3 t = new Vector3(0, 0, 0);
+
+    bool eventler = false;
+    List<FallingObjectClass> mylist = new List<FallingObjectClass>();
+    FallingObjectClass mynewobject;
+
+    void dooo()
+    {
+        mynewobject = new FallingObjectClass(ref1, bar, defaultRedbullCan, flyingRedbulCan, 1.00f, t);
+        mylist.Add(mynewobject);
+    }
+
+    void Start()
+    {
+        InvokeRepeating("dooo", 1.00f, 22.00f);
+        /*Instantiate(defaultRedbullCan, ref1.transform.position, Quaternion.identity, ref1.transform);
+        tempPosition = ref1.transform.position;  */
+    }
+
+    /*void DoThings(RaycastHit hit)
+    {
+
+        eventler = true;
+        Destroy(hit.transform.gameObject);
+        Instantiate(flyingRedbulCan, ref1.transform.position, Quaternion.identity, ref1.transform);
+        //ref1.GetComponent<Animator>().SetBool("end", true);        
+    }*/
+
+    void Update()
+    {
+
+        foreach (FallingObjectClass foc in mylist)
+        {
+            foc.loop();
+        }
+
+        /* if (!eventler)
+         {
+             tempPosition.y += (-1)*Time.deltaTime;
+             ref1.transform.position = tempPosition;
+         }
+         else
+         {
+             var diffofx = bar.transform.position.x - tempPosition.x;
+             var diffofy = bar.transform.position.y - tempPosition.y;
+
+             var distance = Mathf.Sqrt(diffofx*diffofx + diffofy*diffofy);
+
+             tempPosition.x += diffofx*Time.deltaTime / distance;
+             tempPosition.y += diffofy*Time.deltaTime / distance;
+             ref1.transform.position = tempPosition;
+         }
+
+
+         if (Input.GetMouseButtonDown(0))
+         {
+
+             RaycastHit hit;
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+             //Checks whether the mouse click hits a mesh collider
+             if (Physics.Raycast(ray, out hit, 100.0f))
+             {
+                 if (hit.transform != null)
+                 {
+                     DoThings(hit);
+                 }
+             }
+         }*/
+
+
+    }
+}
